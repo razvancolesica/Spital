8c15641d8299810bf861eae86a608dacffd7ec3d
razvancolesica <razvancolesica@gmail.com>
2023-07-19 17:30:01 +0300

    update

---
+++ src/test/java/com/ibmpractica/spital/SpitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmpractica.spital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
